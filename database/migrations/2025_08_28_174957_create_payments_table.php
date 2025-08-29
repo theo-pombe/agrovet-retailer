84502063df09af647d4b7643cd68a5b9d8d87877
theo-pombe <theo.pombe@outlook.com>
2025-08-29 05:25:25 +0300

    feat(payment): add payments table migration

---
+++ database/migrations/2025_08_28_174957_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\PaymentMethod;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('invoice_id')->constrained()->onDelete('cascade');
+
+            $table->decimal('amount', 15, 2);
+            $table->date('payment_date')->useCurrent();
+            $table->enum('payment_method', PaymentMethod::values())->default(PaymentMethod::CASH->value);
+            $table->text('notes')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
