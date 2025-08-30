7ee10f01dd648a9e1d7b619969fb391173fbc99c
theo-pombe <theo.pombe@outlook.com>
2025-08-30 13:43:39 +0300

    feat(seeder): update UnitSeeder

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -13,12 +13,14 @@ class UnitSeeder extends Seeder
             ['name' => 'Piece', 'symbol' => 'pcs', 'is_fractional' => false],
             ['name' => 'Kilogram', 'symbol' => 'kg', 'is_fractional' => true],
             ['name' => 'Gram', 'symbol' => 'g', 'is_fractional' => true],
-            ['name' => 'Liter', 'symbol' => 'L', 'is_fractional' => true],
+            ['name' => 'Liter', 'symbol' => 'ltr', 'is_fractional' => true],
             ['name' => 'Milliliter', 'symbol' => 'ml', 'is_fractional' => true],
             ['name' => 'Box', 'symbol' => 'box', 'is_fractional' => false],
             ['name' => 'Bag', 'symbol' => 'bag', 'is_fractional' => false],
             ['name' => 'Sachet', 'symbol' => 'sachet', 'is_fractional' => false],
             ['name' => 'Pack', 'symbol' => 'pack', 'is_fractional' => false],
+            ['name' => 'Vial', 'symbol' => 'vial', 'is_fractional' => false],
+            ['name' => 'Bottle', 'symbol' => 'bottle', 'is_fractional' => false],
         ];
 
         foreach ($units as $unit) {
